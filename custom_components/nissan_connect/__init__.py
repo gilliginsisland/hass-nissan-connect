93dd4f7dff3fb8f4aef7100635e7ee737ed33c13
Gil Broochian <gil@broochian.com>
2024-07-10 00:32:06 -0400

    Remove unused reference to `hass.data[DOMAIN]`

--- custom_components/nissan_connect/__init__.py
+++ custom_components/nissan_connect/__init__.py
@@ -86,14 +86,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry[RuntimeData]
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(
-        entry, [platform for platform in PLATFORMS]
-    )
-
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
 class TokenStorage():
